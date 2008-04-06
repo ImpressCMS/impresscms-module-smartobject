be1c0bc1f87851791d596c3793c71bf2815966f9
Juan Calzado <debianus@impresscms.org>
2008-04-06 20:30:04 +0000

    Adding Spanish translation for SmartObject
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartobject/trunk@1460 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/spanish/modinfo.php
@@ -0,0 +1,27 @@
+<?php
+//Traducción por debianus. Sugerencias y reporte de errores en http://es.impresscms.org
+/**
+* $Id: modinfo.php 159 2007-12-17 16:44:05Z malanciault $
+* Module: SmartContent
+* Author: The SmartFactory <www.smartfactory.ca>
+* Licence: GNU
+*/
+
+if (!defined("XOOPS_ROOT_PATH")) {
+ 	die("XOOPS root path not defined");
+}
+
+define('_MI_SOBJECT_INDEX', 'Índice');
+define('_MI_SOBJECT_SENT_LINKS', 'Enlaces');
+define('_MI_SOBJECT_TAGS', 'Etiquetas');
+define('_MI_SOBJECT_ADSENSES', '"Adsense ads"');
+define('_MI_SOBJECT_RATINGS', 'Puntuaciones');
+define('_MI_SOBJECT_CURRENCIES', 'Divisas');
+
+define('_MI_SOBJECT_CURRMAN', 'Activar la gestión de divisas');
+define('_MI_SOBJECT_CURRMANDSC', '');
+
+define('_MI_SOBJECT_ADDTO_TITLE', 'Bloque de "Marcadores Sociales"');
+define('_MI_SOBJECT_ADDTO_DESC', '');
+
+?>
