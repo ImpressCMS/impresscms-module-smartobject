6bc246298dc2dae994022798ebd6f4fc8836cca0
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-13 22:14:58 +0000

    modules - SmartObject - portuguesebr - part 4 (modinfo.php) finished...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartobject/trunk@2838 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/modinfo.php
+++ language/portuguesebr/modinfo.php
@@ -12,20 +12,20 @@ if (!defined("XOOPS_ROOT_PATH")) {
  	die("Caminho para o raiz do site não está definido");
 }
 
-define('_MI_SOBJECT_INDEX', 'Index');
+define('_MI_SOBJECT_INDEX', 'Início');
 define('_MI_SOBJECT_SENT_LINKS', 'Links');
-define('_MI_SOBJECT_TAGS', 'Custom tags');
+define('_MI_SOBJECT_TAGS', 'Tags Personalizadas');
 define('_MI_SOBJECT_ADSENSES', 'Adsense ads');
-define('_MI_SOBJECT_RATINGS', 'Ratings');
-define('_MI_SOBJECT_CURRENCIES', 'Currencies');
+define('_MI_SOBJECT_RATINGS', 'Votos');
+define('_MI_SOBJECT_CURRENCIES', 'Moedas');
 
-define('_MI_SOBJECT_CURRMAN', 'Enable currency management');
+define('_MI_SOBJECT_CURRMAN', 'Ativar gestão de moeda');
 define('_MI_SOBJECT_CURRMANDSC', '');
 
-define('_MI_SOBJECT_ADDTO_TITLE', 'AddTo Social bookmarking block');
+define('_MI_SOBJECT_ADDTO_TITLE', 'Adicionar para o bloco Social bookmark');
 define('_MI_SOBJECT_ADDTO_DESC', '');
 
-define('_MI_SOBJECT_ADMFOOTER', 'Enabling admin footer for SmartModules');
-define('_MI_SOBJECT_ADMFOOTERDSC', 'By default SmartModules displays a footer in administrative pages to link on professionnal services available for this module. You can turn off the display of this footer here.');
+define('_MI_SOBJECT_ADMFOOTER', 'Ativar no rodapé do admin para os SmartModules');
+define('_MI_SOBJECT_ADMFOOTERDSC', 'Por padrão o rodapé do SmartModules exibe um link para as páginas administrativas sobre serviços professionais disponíveis para este módulo. Você pode desligar o visor do presente rodapé aqui.');
 
 ?>
