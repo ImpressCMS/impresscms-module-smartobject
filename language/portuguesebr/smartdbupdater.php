b80d9bdc28d8fed44b1987bb75741a4c87a65b08
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-06-13 22:55:57 +0000

    modules - SmartObject - portuguesebr - part 6 (smartdbupdater.php) finished...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartobject/trunk@2840 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/smartdbupdater.php
+++ language/portuguesebr/smartdbupdater.php
@@ -11,25 +11,25 @@ if (!defined("XOOPS_ROOT_PATH")) {
  	die("Caminho para o raiz do site não está definido");
 }
 
-define("_SDU_IMPORT", "Import");
-define("_SDU_CURRENTVER", "Current version: <span class='currentVer'>%s</span>");
-define("_SDU_DBVER", "Database Version %s");
-define("_SDU_MSG_ADD_DATA", "Data added in table %s");
-define("_SDU_MSG_ADD_DATA_ERR", "Error adding data in table %s");
-define("_SDU_MSG_CHGFIELD", "Changing field %s in table %s");
-define("_SDU_MSG_CHGFIELD_ERR", "Error changing field %s in table %s");
-define("_SDU_MSG_CREATE_TABLE", "Table %s created");
-define("_SDU_MSG_CREATE_TABLE_ERR", "Error creating table %s");
-define("_SDU_MSG_NEWFIELD", "Successfully added field %s");
-define("_SDU_MSG_NEWFIELD_ERR", "Error adding field %s");
-define("_SDU_NEEDUPDATE", "Your database is out-of-date. Please upgrade your database tables!<br><b>Note : The SmartFactory strongly recommends you to backup all SmartSection tables before running this upgrade script.</b><br>");
-define("_SDU_NOUPDATE", "Your database is up-to-date. No updates are necessary.");
-define("_SDU_UPDATE_DB", "Updating Database");
-define("_SDU_UPDATE_ERR", "Errors updating to version %s");
-define("_SDU_UPDATE_NOW", "Update Now!");
-define("_SDU_UPDATE_OK", "Successfully updated to version %s");
-define("_SDU_UPDATE_TO", "Updating to version %s");
-define("_SDU_UPDATE_UPDATING_DATABASE", "Updating database...");
-define("_SDU_MSG_DROPFIELD", "Succesffuly droped field %s");
+define("_SDU_IMPORT", "Importar");
+define("_SDU_CURRENTVER", "Versão atual: <span class='currentVer'>%s</span>");
+define("_SDU_DBVER", "Versão do Banco %s");
+define("_SDU_MSG_ADD_DATA", "Dados acrescentado na tabela %s");
+define("_SDU_MSG_ADD_DATA_ERR", "Erro ao adicionar dados na tabela %s");
+define("_SDU_MSG_CHGFIELD", "Mudança de campo %s na tabela %s");
+define("_SDU_MSG_CHGFIELD_ERR", "Erro ao alterar campo %s na tabela %s");
+define("_SDU_MSG_CREATE_TABLE", "Tabela %s criada");
+define("_SDU_MSG_CREATE_TABLE_ERR", "Erro ao criar tabela %s");
+define("_SDU_MSG_NEWFIELD", "Adicionado com sucesso o campo %s");
+define("_SDU_MSG_NEWFIELD_ERR", "Erro ao adicionar o campo %s");
+define("_SDU_NEEDUPDATE", "Sua base de dados está desatualizada. Atualize suas tabelas de banco de dados!<br><b>Nota : O SmartFactory recomenda fortemente que você faça um backup de todas as tabelas antes de executar este script de atualização.</b><br>");
+define("_SDU_NOUPDATE", "Sua base de dados está atualizada. Não são necessárias atualizações.");
+define("_SDU_UPDATE_DB", "Actualização de Dados");
+define("_SDU_UPDATE_ERR", "Erros na atualização para esta versão %s");
+define("_SDU_UPDATE_NOW", "Atualizar agora!");
+define("_SDU_UPDATE_OK", "Versão atualizada com Sucesso %s");
+define("_SDU_UPDATE_TO", "Atualizando a versão %s");
+define("_SDU_UPDATE_UPDATING_DATABASE", "Atualização de dados...");
+define("_SDU_MSG_DROPFIELD", "Campo apagado com Sucesso %s");
 
 ?>
